44b824154e9883c72b3622298cf40ad363f7d5d3
Max Kawula <72083478+Max-Kawula@users.noreply.github.com>
2024-06-30 00:30:13 -0500

    fork time

--- src/gizmo.h
+++ src/gizmo.h
@@ -7,7 +7,6 @@
 
 #ifndef GIZMO_H
 #define GIZMO_H
-
 //eventually be turned into struct members
 #define THRUST 2000.0f //in newtons
 #define WEIGHT 1000.0f //in kilograms
@@ -28,33 +27,26 @@ void updateGizmo(Gizmo* g) {
 	float deltaTime = GetFrameTime();
 	float xAxis = GetGamepadAxisMovement(GAMEPAD_ID, GAMEPAD_AXIS_LEFT_X);
 	float rTrigger = (1.0f+(GetGamepadAxisMovement(GAMEPAD_ID, GAMEPAD_AXIS_RIGHT_TRIGGER)))/2.0f;//some quick maff to turn it into [0..1] range
-	Vector2 steerVector = (Vector2){sinf(g->yaw), cosf(g->yaw)};
-	Vector2 gripVector = (Vector2){sinf(g->yaw - PI/2), cosf(g->yaw - PI/2)}; //the lateral grip unit vector :v
-	float det = 0.0f;
-
+	float det;//these have to do with steering
+	float sine;//TODO should yaw be replaced with a unit vector? discuss.
+	float cosine;
 
 	//VEHICLE ROTATION
-	if( !(FloatEquals(xAxis, 0.0f)) ) {//TODO if statement might be pointless
-		g->yaw += -1.0f*deltaTime*xAxis*STEER_SPEED;
-		g->yaw = Wrap(g->yaw, -PI, PI);
-	}
+	g->yaw += -1.0f*deltaTime*xAxis*STEER_SPEED;
+	g->yaw = Wrap(g->yaw, -PI, PI);
+
+	sine = sinf(g->yaw); //TODO declare locals here instead?
+	cosine = cosf(g->yaw);
 
 	//STEERING PHYSICS
-	//first we take the determinant of Velocity and steerVector
-	det = (g->velocity.x * steerVector.y) - (g->velocity.z * steerVector.x);
-	DrawText(TextFormat("Determinant: %f", det), 20, 65, 10, RED);
-
-	//scale gripVector by this determinant
-	gripVector = Vector2Scale(gripVector, det);//TODO more deltaTime confusion, does steering only apply a fraction of the velocity?
-	//add to velocity!
-	g->velocity = Vector3Add(g->velocity, (Vector3){gripVector.x, 0.0f, gripVector.y});
-
-
+	det = (g->velocity.x * cosine) - (g->velocity.z * sine);//???
+	g->velocity.x += det * -cosine;
+	g->velocity.z += det * sine;
 
 	//APPLY THRUSTER FORCE
 	if(rTrigger > EPSILON) {
-		g->velocity.x += deltaTime*rTrigger*sinf(g->yaw)*sinf(g->yaw)*(THRUST/WEIGHT);
-		g->velocity.z += deltaTime*rTrigger*cosf(g->yaw)*cosf(g->yaw)*(THRUST/WEIGHT);
+		g->velocity.x += deltaTime*rTrigger*sine*(THRUST/WEIGHT);
+		g->velocity.z += deltaTime*rTrigger*cosine*(THRUST/WEIGHT);
 	}
 
 
